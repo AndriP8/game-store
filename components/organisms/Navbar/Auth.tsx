0bc932de68afa249b184c96dd24cb4f74a5793e1
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-10-17 22:00:50 +0700

    add navbar organism in atomic design

---
+++ components/organisms/Navbar/Auth.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface AuthProps {
+  isLogin: boolean;
+}
+
+function Auth(props: Partial<AuthProps>) {
+  const { isLogin } = props;
+  if (isLogin) {
+    return (
+      <li className="nav-item my-auto dropdown d-flex">
+        <div className="vertical-line d-lg-block d-none" />
+        <div>
+          <a className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            <Image src="/img/avatar-1.png" className="rounded-circle" width={40} height={40} alt="avatar-user" />
+          </a>
+
+          <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
+            <li>
+              <Link href="/member">
+                <a className="dropdown-item text-lg color-palette-2">My Profile</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/wallet">
+                <a className="dropdown-item text-lg color-palette-2">Wallet</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/member/edit-profile">
+                <a className="dropdown-item text-lg color-palette-2">Account Settings</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/sign-in">
+                <a className="dropdown-item text-lg color-palette-2">Log Out</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </li>
+    );
+  }
+  return (
+    <li className="nav-item my-auto">
+      <Link href="/sign-in">
+        <a className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill" role="button">
+          Sign In
+        </a>
+      </Link>
+    </li>
+  );
+}
+
+export default Auth;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import "../styles/homepage.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
