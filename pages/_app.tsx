0f596dd740801f0c75677ae69024941fc340663e
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-10-18 14:03:15 +0700

    add all component organisms to root page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import "../styles/homepage.css";
+import "../styles/utilities.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
