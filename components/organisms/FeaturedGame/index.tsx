61486c0dc5c3445f7d7646457aa0a9a8ff8ef74a
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-10-21 22:45:57 +0700

    update path url thumbnail

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -10,11 +10,11 @@ function FeaturedGame() {
           Games This Year
         </h2>
         <div className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4" data-aos="fade-up">
-          <GameItem thumbnail="Thumbnail-1" title="Super Mechs" category="Mobile" />
-          <GameItem thumbnail="Thumbnail-2" title="Call of Duty: Modern" category="Mobile" />
-          <GameItem thumbnail="Thumbnail-3" title="Mobile Legends" category="Mobile" />
-          <GameItem thumbnail="Thumbnail-4" title="Clash of Clans" category="Mobile" />
-          <GameItem thumbnail="Thumbnail-5" title="Valorant" category="Dekstop" />
+          <GameItem thumbnail="/img/Thumbnail-1.png" title="Super Mechs" category="Mobile" />
+          <GameItem thumbnail="/img/Thumbnail-2.png" title="Call of Duty: Modern" category="Mobile" />
+          <GameItem thumbnail="/img/Thumbnail-3.png" title="Mobile Legends" category="Mobile" />
+          <GameItem thumbnail="/img/Thumbnail-4.png" title="Clash of Clans" category="Mobile" />
+          <GameItem thumbnail="/img/Thumbnail-5.png" title="Valorant" category="Dekstop" />
         </div>
       </div>
     </section>
