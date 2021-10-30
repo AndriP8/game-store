a13a72f21591208b29494aabab432f511f2e7247
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-10-30 22:34:36 +0700

    add integration api Detail Page

---
+++ pages/detail/[id].tsx
@@ -0,0 +1,66 @@
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import Footer from "../../components/organisms/Footer";
+import TopUpForm from "../../components/organisms/TopUpForm";
+import TopUpItem from "../../components/organisms/TopUpItems";
+import Navbar from "../../components/organisms/Navbar";
+import { getVoucherDetail } from "../../services/player";
+
+function Detail() {
+  const { query, isReady } = useRouter();
+
+  const [dataItem, setDataItem] = useState({
+    name: "",
+    thumbnail: "",
+    category: {
+      name: "",
+    },
+  });
+
+  const [nominals, setNominals] = useState([]);
+  const [payments, setPayments] = useState([]);
+
+  const getVoucherDetailApi = useCallback(async (id) => {
+    const data = await getVoucherDetail(id);
+    setDataItem(data.detail);
+    setNominals(data.detail.nominals);
+    setPayments(data.payment);
+  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      getVoucherDetailApi(query.id);
+    }
+  }, [isReady]);
+
+  return (
+    <>
+      <Navbar />
+      <section className="detail pt-lg-60 pb-50">
+        <div className="container-xxl container-fluid">
+          <div className="detail-header pb-50">
+            <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10">
+              Top Up
+            </h2>
+            <p className="text-lg color-palette-1 mb-0">
+              Perkuat akun dan jadilah pemenang
+            </p>
+          </div>
+          <div className="row">
+            <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
+              <TopUpItem type="mobile" data={dataItem} />
+            </div>
+            <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
+              <TopUpItem type="dekstop" data={dataItem} />
+              <hr />
+              <TopUpForm nominals={nominals} payments={payments} />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default Detail;
