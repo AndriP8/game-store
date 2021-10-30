eeff60211cd6de2dc6c98504975fbca21e65d602
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-10-30 22:24:20 +0700

    add setup Api call

---
+++ services/player.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const ROOT_API = process.env.NEXT_PUBLIC_API;
+const API_VERSION = "api/v1";
+
+export const getFeaturedGame = async () => {
+  const URL = "players/landingpage";
+
+  const response = await axios.get(`${ROOT_API}/${API_VERSION}/${URL}`);
+  const axiosResponse = await response.data;
+
+  return axiosResponse.data;
+};
