60e351865ea9c811b19159f2f6f3703423942807
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-01 14:04:35 +0700

    update private route page Checkout using ssr

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import jwtDecode from "jwt-decode";
+import { JWTPayloadTypes, UserTypes } from "../services/data-types";
 import CheckoutConfirmation from "../components/organisms/CheckoutConfirmation";
 import CheckoutDetail from "../components/organisms/CheckoutDetail";
 import CheckoutItem from "../components/organisms/CheckoutItem";
 
-function Checkout() {
+interface CheckoutProps {
+  user: UserTypes;
+}
+
+function Checkout(props: CheckoutProps) {
+  const { user } = props;
+
   return (
     <section className="checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30">
       <div className="container-fluid">
@@ -17,7 +25,9 @@ function Checkout() {
         </div>
         <div className="title-text pt-md-50 pt-0">
           <h2 className="text-4xl fw-bold color-palette-1 mb-10">Checkout</h2>
-          <p className="text-lg color-palette-1 mb-0">Waktunya meningkatkan cara bermain</p>
+          <p className="text-lg color-palette-1 mb-0">
+            Waktunya meningkatkan cara bermain
+          </p>
         </div>
         <CheckoutItem />
         <hr />
@@ -29,3 +39,28 @@ function Checkout() {
 }
 
 export default Checkout;
+
+export async function getServerSideProps({ req }) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+  }
+
+  const jwtToken = Buffer.from(token, "base64").toString("ascii");
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const userFromPayload: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMAGE;
+  userFromPayload.avatar = `${IMG}/${userFromPayload.avatar}`;
+
+  return {
+    props: {
+      user: userFromPayload,
+    },
+  };
+}
