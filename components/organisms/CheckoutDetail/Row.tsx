82baae0de56eeb56b5c360c7f78d59a0d408b5a2
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-08 14:49:32 +0700

    add integration checkout detail using localStorage

--- components/organisms/CheckoutDetail/Row.tsx
+++ components/organisms/CheckoutDetail/Row.tsx
@@ -1,16 +1,31 @@
+import NumberFormat from "react-number-format";
+
 interface RowProps {
   label: string;
   value: string | number;
   className?: string;
+  numberFormat: boolean;
 }
 
 function Row(props: Partial<RowProps>) {
-  const { label, value, className } = props;
+  const { label, value, className, numberFormat } = props;
 
   return (
     <p className="text-lg color-palette-1 mb-20">
       {label}
-      <span className={`purchase-details ${className}`}>{value}</span>
+      {numberFormat ? (
+        <span className={`purchase-details ${className}`}>
+          <NumberFormat
+            value={value}
+            prefix="Rp "
+            displayType="text"
+            thousandSeparator="."
+            decimalSeparator=","
+          />
+        </span>
+      ) : (
+        <span className={`purchase-details ${className}`}>{value}</span>
+      )}
     </p>
   );
 }
--- components/organisms/CheckoutDetail/index.tsx
+++ components/organisms/CheckoutDetail/index.tsx
@@ -1,24 +1,75 @@
+import { useEffect, useState } from "react";
+
 import Row from "./Row";
 
 function CheckoutDetail() {
+  const [dataTopUp, setDataTopUp] = useState({
+    verifyID: "",
+    nominalItem: {
+      coinName: "",
+      coinQuantity: "",
+      price: 0,
+    },
+    paymentItem: {
+      payment: {
+        id: "",
+        type: "",
+      },
+      bank: {
+        bankName: "",
+        name: "",
+        noRekening: "",
+      },
+    },
+    bankAccountName: "",
+  });
+
+  useEffect(() => {
+    const dataFromLocal = localStorage.getItem("data-top-up");
+    setDataTopUp(JSON.parse(dataFromLocal!));
+  }, []);
+
+  const itemPrice = dataTopUp.nominalItem.price;
+
+  const tax = dataTopUp.nominalItem.price * (10 / 100);
+  const totalPrice = itemPrice + tax;
+
   return (
     <>
       <div className="purchase pt-md-50 pt-30">
-        <h2 className="fw-bold text-xl color-palette-1 mb-20">Purchase Details</h2>
-        <Row label="Your Game ID" value="masayoshizero" />
+        <h2 className="fw-bold text-xl color-palette-1 mb-20">
+          Purchase Details
+        </h2>
+        <Row label="Your Game ID" value={dataTopUp.verifyID} />
         <Row label="Order ID" value="#GG001" />
-        <Row label="Item" value="250 Diamonds" />
-        <Row label="Price" value={42280000} />
-        <Row label="Tax (10%)" value={4228000} />
-        <Row label="Total" value={550000600} className="color-palette-4" />
+        <Row
+          label="Item"
+          value={`${dataTopUp.nominalItem.coinQuantity} ${dataTopUp.nominalItem.coinName}`}
+        />
+        <Row label="Price" value={itemPrice} numberFormat />
+        <Row label="Tax (10%)" value={tax} numberFormat />
+        <Row
+          label="Total"
+          value={totalPrice}
+          className="color-palette-4"
+          numberFormat
+        />
       </div>
       <div className="payment pt-md-50 pb-md-50 pt-10 pb-10">
-        <h2 className="fw-bold text-xl color-palette-1 mb-20">Payment Informations</h2>
-        <Row label="Your Account Name" value="Masayoshi Angga Zero" />
-        <Row label="Type" value="Worldwide Transfer" />
-        <Row label="Bank Name" value="Mandiri" />
-        <Row label="Bank Account Name" value="PT Store GG Indonesia" />
-        <Row label="Bank Number" value="1800 - 9090 - 2021" />
+        <h2 className="fw-bold text-xl color-palette-1 mb-20">
+          Payment Informations
+        </h2>
+        <Row label="Your Account Name" value={dataTopUp.bankAccountName} />
+        <Row label="Type" value={dataTopUp.paymentItem.payment.type} />
+        <Row label="Bank Name" value={dataTopUp.paymentItem.bank.bankName} />
+        <Row
+          label="Bank Account Name"
+          value={dataTopUp.paymentItem.bank.name}
+        />
+        <Row
+          label="Bank Number"
+          value={dataTopUp.paymentItem.bank.noRekening}
+        />
       </div>
     </>
   );
