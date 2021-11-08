e4a3b6e9c6aeb5b75c0e3ffe008c907215e438b0
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-08 14:44:01 +0700

    update headers config callAPI

--- config/api/index.ts
+++ config/api/index.ts
@@ -1,10 +1,27 @@
 import axios, { AxiosRequestConfig } from "axios";
+import Cookies from "js-cookie";
 
-const callAPI = async ({ url, method, data }: AxiosRequestConfig) => {
+interface CallAPIProps extends AxiosRequestConfig {
+  token?: boolean;
+}
+
+const callAPI = async ({ url, method, data, token }: CallAPIProps) => {
+  let headers = {};
+  if (token) {
+    const tokenCookies = Cookies.get("token");
+    if (tokenCookies) {
+      const jwtToken = atob(tokenCookies);
+      headers = {
+        Authorization: `Bearer ${jwtToken}`,
+      };
+    }
+  }
+
   const response = await axios({
     url,
     method,
     data,
+    headers,
   }).catch((err) => err.response);
 
   if (response.status > 300) {
