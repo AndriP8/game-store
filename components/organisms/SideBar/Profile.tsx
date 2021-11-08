a2d71cdc33b5c8b8136125d6f654896061c2aadb
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-08 22:10:21 +0700

    update profile member using token payload

--- components/organisms/SideBar/Profile.tsx
+++ components/organisms/SideBar/Profile.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
 import Image from "next/image";
+import { JWTPayloadTypes, UserTypes } from "../../../services/data-types";
 
 function Profile() {
+  const [user, setuser] = useState({
+    avatar: "/img/avatar.png",
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userFromPayload: UserTypes = payload.player;
+      setuser(userFromPayload);
+    }
+  }, []);
+
   return (
     <div className="user text-center pb-50 pe-30">
-      <Image src="/img/avatar-1.png" width={90} height={90} className="img-fluid mb-20" alt="profile" />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <Image
+        src={user.avatar}
+        width={90}
+        height={90}
+        className="img-fluid mb-20 rounded-circle"
+        alt="profile"
+      />
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+      <p className="color-palette-2 m-0">{user.email}</p>
     </div>
   );
 }
