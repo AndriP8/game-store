4496908753b8c3ef018a6a5e26941edd4facf1db
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-09 21:30:31 +0700

    add logout handler in sidebar

--- components/organisms/SideBar/MenuItem.tsx
+++ components/organisms/SideBar/MenuItem.tsx
@@ -4,27 +4,44 @@ import Link from "next/link";
 
 interface MenuItemProps {
   title: string;
-  icon: "overview" | "transaction" | "messages" | "card" | "rewards" | "setting" | "logout";
+  icon:
+    | "overview"
+    | "transaction"
+    | "messages"
+    | "card"
+    | "rewards"
+    | "setting"
+    | "logout";
   active?: boolean;
-  href: string;
+  href?: string;
+  onClick?: () => void;
 }
 
 function MenuItem(props: Partial<MenuItemProps>) {
-  const { title, icon, active, href } = props;
+  const { title, icon, active, href, onClick } = props;
   const classItem = cx({
     item: true,
     "mb-30": true,
     active,
   });
   return (
-    <div className={classItem}>
+    <div className={classItem} onClick={onClick}>
       <div className="me-3">
         <Image src={`/icon/icon-menu-${icon}.svg`} width={25} height={25} />
       </div>
       <p className="item-title m-0">
-        <Link href={href}>
-          <a className="text-lg text-decoration-none">{title}</a>
-        </Link>
+        {!onClick ? (
+          <Link href={href}>
+            <a className="text-lg text-decoration-none">{title}</a>
+          </Link>
+        ) : (
+          <a
+            className="text-lg text-decoration-none"
+            style={{ cursor: "pointer" }}
+          >
+            {title}
+          </a>
+        )}
       </p>
     </div>
   );
--- components/organisms/SideBar/index.tsx
+++ components/organisms/SideBar/index.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 import Footer from "./Footer";
 import Profile from "./Profile";
 import MenuItem from "./MenuItem";
@@ -8,18 +10,41 @@ interface SidebarProps {
 
 function Sidebar(props: SidebarProps) {
   const { activeMenu } = props;
+
+  const router = useRouter();
+
+  const onLogout = () => {
+    Cookies.remove("token");
+    router.push("/sign-in");
+  };
+
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem title="Overview" icon="overview" href="/member" active={activeMenu === "overview"} />
-          <MenuItem title="Transactions" icon="transaction" href="/member/transactions" active={activeMenu === "transactions"} />
+          <MenuItem
+            title="Overview"
+            icon="overview"
+            href="/member"
+            active={activeMenu === "overview"}
+          />
+          <MenuItem
+            title="Transactions"
+            icon="transaction"
+            href="/member/transactions"
+            active={activeMenu === "transactions"}
+          />
           <MenuItem title="Messages" icon="messages" href="/member" />
           <MenuItem title="Card" icon="card" href="/member" />
           <MenuItem title="Rewards" icon="rewards" href="/member" />
-          <MenuItem title="Settings" icon="setting" href="/member/edit-profile" active={activeMenu === "settings"} />
-          <MenuItem title="Logout" icon="logout" href="/sign-in" />
+          <MenuItem
+            title="Settings"
+            icon="setting"
+            href="/member/edit-profile"
+            active={activeMenu === "settings"}
+          />
+          <MenuItem title="Logout" icon="logout" onClick={onLogout} />
         </div>
         <Footer />
       </div>
