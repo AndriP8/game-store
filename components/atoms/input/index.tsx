ecc08dc87d77bf94c1a94ac0cdff63186094c964
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-11 21:48:58 +0700

    add integrasi api update profile

--- components/atoms/input/index.tsx
+++ components/atoms/input/index.tsx
@@ -1,19 +1,26 @@
-export interface InputFormProps {
+import { InputHTMLAttributes } from "react";
+
+export interface InputFormProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   labelFor: string;
   type: string;
   placeholder: string;
+  value?: string;
 }
 
 function InputForm(props: InputFormProps) {
-  const { label, labelFor, placeholder, type, ...nativeProps } = props;
+  const { label, labelFor, placeholder, type, value, ...nativeProps } = props;
   return (
     <div className="pt-30">
-      <label htmlFor={labelFor} className="form-label text-lg fw-medium color-palette-1 mb-10">
+      <label
+        htmlFor={labelFor}
+        className="form-label text-lg fw-medium color-palette-1 mb-10"
+      >
         {label}
       </label>
       <input
         type={type}
+        value={value}
         className="form-control rounded-pill text-lg"
         id={labelFor}
         name={labelFor}
--- pages/member/edit-profile.tsx
+++ pages/member/edit-profile.tsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+import { toast } from "react-toastify";
+import jwtDecode from "jwt-decode";
 import InputForm from "../../components/atoms/input";
 import Sidebar from "../../components/organisms/SideBar";
+import { JWTPayloadTypes, UserTypes } from "../../services/data-types";
+import { updateProfile } from "../../services/member";
 
 function EditProfile() {
+  const [user, setuser] = useState({
+    _id: "",
+    name: "",
+    email: "",
+    phoneNumber: "",
+    avatar: "",
+  });
+
+  const [imagePreview, setImagePreview] = useState("/icon/upload.svg");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    const jwtToken = atob(token);
+    const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+    const userFromPayload: UserTypes = payload.player;
+    setuser(userFromPayload);
+    setImagePreview(userFromPayload?.avatar);
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("image", user.avatar);
+    data.append("name", user.name);
+
+    const response = await updateProfile(data);
+    if (response.error) {
+      toast.error(response.message);
+    } else {
+      Cookies.remove("token");
+      router.push("/sign-in");
+    }
+  };
+
   return (
     <section className="edit-profile overflow-auto">
       <Sidebar activeMenu="settings" />
@@ -12,24 +55,60 @@ function EditProfile() {
           <div className="bg-card pt-30 ps-30 pe-30 pb-30">
             <form action="">
               <div className="photo d-flex">
-                <div className="position-relative me-20">
-                  <Image src="/img/avatar-1.png" width={90} height={90} className="avatar img-fluid" alt="avatar" />
-                  <div className="avatar-overlay position-absolute top-0 d-flex justify-content-center align-items-center">
-                    <Image src="/icon/upload.svg" height={24} width={24} alt="icon-upload" />
-                  </div>
-                </div>
                 <div className="image-upload">
                   <label htmlFor="avatar">
-                    <Image src="/icon/upload.svg" height={90} width={90} alt="icon-upload" />
+                    {imagePreview ? (
+                      <Image
+                        src={imagePreview}
+                        width={90}
+                        height={90}
+                        className="avatar img-fluid rounded-circle"
+                        alt="avatar"
+                      />
+                    ) : (
+                      <Image
+                        src={user?.avatar}
+                        width={90}
+                        height={90}
+                        className="avatar img-fluid rounded-circle "
+                        alt="avatar"
+                      />
+                    )}
                   </label>
-                  <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                  <input
+                    id="avatar"
+                    type="file"
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={(e) => {
+                      const img = e.target.files[0];
+                      setImagePreview(URL.createObjectURL(img));
+                      return setuser({ ...user, avatar: img });
+                    }}
+                  />
                 </div>
               </div>
-              <InputForm label="Full Name" labelFor="name" type="text" placeholder="Enter your name" />
-              <InputForm label="Email Addres" labelFor="email" type="email" placeholder="Enter your email address" />
-              <InputForm label="Phone" labelFor="phone" type="tel" placeholder="Enter your phone number" />
+              <InputForm
+                label="Full Name"
+                labelFor="name"
+                type="text"
+                placeholder="Enter your name"
+                value={user.name}
+                onChange={(e) => setuser({ ...user, name: e.target.value })}
+              />
+              <InputForm
+                label="Email Addres"
+                labelFor="email"
+                type="email"
+                placeholder="Enter your email address"
+                disabled
+              />
               <div className="button-group d-flex flex-column pt-50">
-                <button type="submit" className="btn btn-save fw-medium text-lg text-white rounded-pill" role="button">
+                <button
+                  type="submit"
+                  className="btn btn-save fw-medium text-lg text-white rounded-pill"
+                  onClick={onSubmit}
+                >
                   Save My Profile
                 </button>
               </div>
