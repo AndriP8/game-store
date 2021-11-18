a7ba708556a79308f933a45fcc366a98bb22143a
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-18 14:07:54 +0700

    setup basic seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import type { NextPage } from "next";
+import Head from "next/head";
 import Navbar from "../components/organisms/Navbar";
 import MainBanner from "../components/organisms/MainBanner";
 import TransactionStep from "../components/organisms/TransactionStep";
@@ -15,6 +16,22 @@ const Home: NextPage = () => {
   }, []);
   return (
     <>
+      <Head>
+        <title>GameStore | Get a New Experience in Gaming</title>
+        <meta
+          name="description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta
+          property="og:title"
+          content="GameStore | Get a New Experience in Gaming"
+        />
+        <meta
+          property="og:description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta property="og:url" content="https://gamestore.vercel.app" />
+      </Head>
       <Navbar />
       <MainBanner />
       <TransactionStep />
